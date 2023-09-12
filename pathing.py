2d094bcf6644fab5cf009997f7f869174b7dccfe
megahomyak <g.megahomyak@gmail.com>
2023-09-12 10:19:35 +0500

    Made this basic thingy

--- pathing.py
+++ pathing.py
@@ -1,26 +1,37 @@
 from dataclasses import dataclass
-from typing import Iterable
+import os
+from typing import List
+from PIL import Image
+import json
 
 PointId = str
+PlaneId = str
 
 @dataclass
 class Point:
     x: int
     y: int
     floor: int
-    linked: Iterable[PointId]
+    linked: List[PointId]
+    plane: PlaneId
+
+@dataclass
+class Plane:
+    background: Image.Image
+    foreground: Image.Image
+
+def open_image(file_name):
+    return Image.open(os.path.join("resources/images", file_name))
+
+planes = {
+    id_: Plane(
+        background=open_image(contents["background"]),
+        foreground=open_image(contents["foreground"]),
+    )
+    for id_, contents in json.load(open("resources/planes.json", encoding="utf-8"))
+}
 
 points = {
-    "Name": Point(
-        x=100,
-        y=100,
-        floor=1,
-        linked=["Name2"],
-    ),
-    "Name2": Point(
-        x=300,
-        y=200,
-        floor=1,
-        linked=["Name"],
-    ),
+    id_: Point(**contents)
+    for id_, contents in json.load(open("resources/points.json", encoding="utf-8"))
 }
